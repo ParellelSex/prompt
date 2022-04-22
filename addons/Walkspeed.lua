d3544a2b0edda707bb247d02c5233b4618eee974
PeaPattern <72760536+PeaPattern@users.noreply.github.com>
2022-04-22 17:58:13 -0400

    just walkspeed challenge
    hi

--- addons/Walkspeed.lua
+++ addons/Walkspeed.lua
@@ -18,22 +18,3 @@ addCommand({"walkspeed", "ws", "speed"}, "Sets players walkspeed.", function(Mes
 		return "Invalid argument.\n"
 	end
 end)
-
-addCommand({"jumppower", "jp"}, "Sets players jumppower", function(Message, Args)
-	if #Args >= 1 then
-		local JumpPower = Args[1]
-		if tonumber(JumpPower) then
-			local Humanoid = getHumanoid()
-			if Humanoid then
-				Player.Character:FindFirstChildOfClass("Humanoid").JumpPower = tonumber(JumpPower)
-				return "Successfully set jumppower to " .. JumpPower .. "\n"
-			else
-				return "Player has no humanoid.\n"
-			end
-		else
-			return "Argument is not a number: " .. JumpPower .. "\n"
-		end
-	else
-		return "Invalid argument.\n"
-	end
-end)

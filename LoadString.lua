f82747dcbafa1e85adda93a0761ea78dd1ced753
ParellelSex <88294148+ParellelSex@users.noreply.github.com>
2022-04-22 15:21:20 -0500

    Create LoadString.lua

---
+++ LoadString.lua
@@ -0,0 +1,12 @@
+repeat task.wait() until game:IsLoaded() == true
+
+local user = 'ParellelSex'
+local branch = 'prompt'
+local link = 'https://raw.githubusercontent.com/'
+
+local function GetURL(url)
+    local get = '%s/%s/%s/main/%s'
+    return game:HttpGet(string.format(get,link,user,branch,url))
+end
+
+loadstring(GetURL('PromptMain.lua'))();

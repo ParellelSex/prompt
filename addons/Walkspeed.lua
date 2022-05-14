f4965e8fcd4ca8a30869129b443277b73b4ece14
PeaPattern <72760536+PeaPattern@users.noreply.github.com>
2022-05-14 12:19:41 -0400

    cleaner
    guard clause or whatever, just learned and it looks so good

--- addons/Walkspeed.lua
+++ addons/Walkspeed.lua
@@ -1,20 +1,19 @@
 local Player = game:GetService("Players").LocalPlayer
 
 addCommand({"walkspeed", "ws", "speed"}, "Sets players walkspeed.", function(Message, Args)
-	if #Args >= 1 then
-		local Speed = Args[1]
-		if tonumber(Speed) then
-			local Humanoid = getHumanoid()
-			if Humanoid then
-				Player.Character:FindFirstChildOfClass("Humanoid").WalkSpeed = tonumber(Speed)
-				return "Successfully set speed to " .. Speed .. "\n"
-			else
-				return "Player has no humanoid.\n"
-			end
-		else
-			return "Argument is not a number: " .. Speed .. "\n"
-		end
-	else
+	if #Args < 1 then
 		return "Invalid argument.\n"
 	end
+	
+	local Speed = Args[1]
+	if not tonumber(Speed) then
+		return "Argument is not a number: " .. Speed .. "\n"
+	end
+	
+	local Humanoid = getHumanoid()
+	if not Humanoid then
+		return "Player has no humanoid.\n"
+	end
+	
+	Humanoid.WalkSpeed = tonumber(Speed)
 end)
